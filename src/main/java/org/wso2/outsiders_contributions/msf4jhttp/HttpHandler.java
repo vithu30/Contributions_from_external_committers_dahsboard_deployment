e409ff34b18c615a55aaeb5166d7abee74515a6c
Vithursa <vithu30@hotmail.com>
2018-03-26 10:48:55 +0530

    Version changed

--- src/main/java/org/wso2/outsiders_contributions/msf4jhttp/HttpHandler.java
+++ src/main/java/org/wso2/outsiders_contributions/msf4jhttp/HttpHandler.java
@@ -19,6 +19,7 @@
 
 package org.wso2.outsiders_contributions.msf4jhttp;
 
+import com.google.common.io.BaseEncoding;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
@@ -30,7 +31,6 @@ import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.Base64;
 
 
 /*
@@ -110,7 +110,7 @@ public class HttpHandler {
     private String encode(String text) {
         String returnString = null;
         try {
-            returnString = Base64.getEncoder().encodeToString(text.getBytes("utf-8"));
+            returnString = BaseEncoding.base64().encode(text.getBytes("utf-8"));
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
@@ -118,3 +118,4 @@ public class HttpHandler {
     }
 
 }
+
