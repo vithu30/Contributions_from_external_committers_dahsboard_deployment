24cb2624a9a83805629ea9e74b188fd1e38a6fe1
Vithursa <vithu30@hotmail.com>
2018-03-26 09:56:39 +0530

    First commit

---
+++ src/main/java/org/wso2/outsiders_contributions/serverlets/PullRequests.java
@@ -0,0 +1,33 @@
+package org.wso2.outsiders_contributions.serverlets;
+
+import java.io.IOException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.log4j.Logger;
+import org.wso2.outsiders_contributions.msf4jhttp.HttpHandler;
+
+@WebServlet(
+        name = "pullRequests",
+        urlPatterns = "/pullRequests"
+)
+public class PullRequests extends HttpServlet {
+    private static final Logger logger = Logger.getLogger(PullRequests.class);
+
+    @Override
+        protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse){
+        try {
+            HttpHandler httpHandler  = new HttpHandler();
+            logger.info("Request backend to fetch pull requests");
+            String response = httpHandler.get("BallerinaService/pullRequests");
+            logger.info("Got: " + response);
+            httpServletResponse.setContentType("application/json;charset=UTF-8");
+            ServletOutputStream out = httpServletResponse.getOutputStream();
+            out.print(response);
+        }catch (IOException e){
+            logger.error("The response output stream failed");
+        }
+    }
+}
